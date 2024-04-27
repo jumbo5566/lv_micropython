509ead90edaba31fca5200e47dcfd84f57fa59b9
iotah <writeforever@foxmail.com>
2024-04-28 03:39:10 +0800

    build script add other board
    Signed-off-by: iotah <writeforever@foxmail.com>

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 git submodule update --init --recursive lib/lv_bindings
 make -C ports/rp2 BOARD=PICO submodules
 make -j -C mpy-cross
-make -j -C ports/rp2 BOARD=PICO USER_C_MODULES=../../lib/lv_bindings/bindings.cmake
+# make -j -C ports/rp2 BOARD=PICO USER_C_MODULES=../../lib/lv_bindings/bindings.cmake
+make -j -C ports/rp2 BOARD=PICO DISP_BOARD=PICO_DM_FPC032MRA003 USER_C_MODULES=../../lib/lv_bindings/bindings.cmake
 
 ln -sf ports/rp2/build-PICO/compile_commands.json .
